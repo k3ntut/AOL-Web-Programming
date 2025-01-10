b224297059db81233aea6a995e89b8f43fa0fe0f
k3ntut <kenzieharsanto123@gmail.com>
2025-01-10 19:25:38 +0700

    ans

--- public/index.php
+++ public/index.php
@@ -4,14 +4,23 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Check for Maintenance Mode
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// Composer Autoload
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// Bootstrap Laravel
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Create a Kernel Instance
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+// Capture and Handle the Request
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
